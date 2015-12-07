a559cbc36dedbbc1541a01859b26b55b9d291a36
Kevin Mooney <kmooney517@gmail.com>
2015-12-07 16:13:37 -0500

    worked on sorting items in pantry by category and adding number of items in category to the side

--- app/js/main.js
+++ app/js/main.js
@@ -387,44 +387,75 @@ var UserHomeController = function UserHomeController($cookies, ListService, Pant
   groceryList();
   function groceryList() {
     ListService.getGroceryList().then(function (response) {
-      // console.log(response);
       vm.items = response.data;
     });
   }
+
+  vm.necessity = [];
+  vm.produce = [];
+  vm.dairy = [];
+  vm.deli = [];
+  vm.meats = [];
+  vm.baking = [];
+  vm.spices = [];
+  vm.frozen = [];
+  vm.bevs = [];
+  vm.hygiene = [];
+  vm.household = [];
+  vm.other = [];
+
   pantryList();
+
   function pantryList() {
     PantryService.getPantryList().then(function (response) {
-      // console.log(response);
       vm.pantryItems = response.data;
+      var items = response.data;
+      items.forEach(function (item) {
+        if (item.neccessity === true) {
+          vm.necessity.push(item);
+        } else if (item.category === "Produce") {
+          vm.produce.push(item);
+          vm.produceAmt = vm.produce.length;
+        } else if (item.category === "Dairy") {
+          vm.dairy.push(item);
+          vm.dairyAmt = vm.dairy.length;
+        } else if (item.category === "Deli") {
+          vm.deli.push(item);
+          vm.deliAmt = vm.deli.length;
+        } else if (item.category === "Meats") {
+          vm.meats.push(item);
+          vm.meatsAmt = vm.meats.length;
+        } else if (item.category === "Baking") {
+          vm.baking.push(item);
+          vm.bakingAmt = vm.baking.length;
+        } else if (item.category === "Spices") {
+          vm.spices.push(item);
+          vm.spicesAmt = vm.spices.length;
+        } else if (item.category === "Frozen") {
+          vm.frozen.push(item);
+          vm.frozenAmt = vm.frozen.length;
+        } else if (item.category === "Beverages") {
+          vm.bevs.push(item);
+          vm.bevsAmt = vm.bevs.length;
+        } else if (item.category === "Hygiene") {
+          vm.hygiene.push(item);
+          vm.hygieneAmt = vm.hygiene.length;
+        } else if (item.category === "Household") {
+          vm.household.push(item);
+          vm.householeAmt = vm.household.length;
+        } else {
+          vm.other.push(item);
+          vm.otherAmt = vm.other.length;
+        }
+      });
     });
   }
-  $scope.sort = {
-    column: '',
-    descending: false
-  };
 
-  vm.sortBy = function (column) {
-
-    var sort = $scope.sort;
-
-    if (sort.column === column) {
-      sort.descending = !sort.descending;
-    } else {
-      sort.column = column;
-      sort.descending = false;
-    }
-  };
-
-  vm.sortPantry = function (column) {
-
-    var sort = $scope.sort;
-
-    if (sort.column === column) {
-      sort.descending = !sort.descending;
-    } else {
-      sort.column = column;
-      sort.descending = false;
-    }
+  vm.logOut = function () {
+    $cookies.remove('auth_token');
+    $cookies.remove('username');
+    $cookies.remove('house_id');
+    $state.go('landing');
   };
 };
 
